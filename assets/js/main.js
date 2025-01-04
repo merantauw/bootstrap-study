2d213770bfdcf8feb6e152e3f45db42c89f927ed
Merantauw <zholu.vlad2010@yandex.ru>
2025-01-04 20:06:38 +0500

    сделал offcanvas-корзину + поправил микробаги

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,25 @@
 window.addEventListener('scroll', function() {
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
-})
+});
 
 const offcanvasCartEl = document.getElementById('offCanvasCart');
 const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
 
-document.querySelectorAll('.closecart').forEach(item => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-        offcanvasCart.hide();
-        // let href = item.href.split('#').pop();
-        let href = item.dataset.href;
-        offcanvasCartEl.addEventListener('hidden.bs.offcanvas', () => {
-            document.getElementById(href).scrollIntoView();
-        })
-    });
+document.getElementById('cart-open').addEventListener('click', e => {
+    e.preventDefault();
+    offcanvasCart.toggle();
 });
+
+document.querySelectorAll('.closecart').forEach(item => {
+   item.addEventListener('click', (e) =>{
+      e.preventDefault();
+      offcanvasCart.hide();
+
+      let href = item.dataset.href;
+      document.getElementById(href).scrollIntoView();
+   });
+});
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop() > 400) {
