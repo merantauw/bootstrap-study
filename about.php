b93caabbecb19f131c3b993df5e945abf4d688d7
Merantauw <zholu.vlad2010@yandex.ru>
2025-01-08 21:30:49 +0500

    started project

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+
+$title = 'My Blog:: About';
+
+$post = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam cum, deleniti dolorem dolores enim eum inventore
+    laudantium nobis officia omnis quidem rerum sapiente sed similique soluta tempora, veniam vero.</p>
+    <p>Alias aperiam architecto asperiores autem blanditiis, dicta dolores eligendi explicabo facere incidunt nam nulla porro quae quo
+    tenetur voluptate voluptatem. Cumque excepturi itaque labore nobis quo. Aut corporis nisi veniam!</p>
+    <p>Consectetur culpa doloribus ex fuga maxime quia quibusdam repellendus tempore unde vero. Animi autem blanditiis culpa, cum dicta
+    dignissimos doloremque dolores excepturi hic itaque, laudantium mollitia tempora, vel veniam voluptatum?</p>';
+
+$recent_posts = [
+    1 => [
+        'title' => 'An item',
+        'slug' => lcfirst(str_replace(' ', '-', 'An item')),
+    ],
+    2 => [
+        'title' => 'A second item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A second item')),
+    ],
+    3 => [
+        'title' => 'A third item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A third item')),
+    ],
+    4 => [
+        'title' => 'A fourth item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A fourth item')),
+    ],
+    5 => [
+        'title' => 'And a fifth one',
+        'slug' => lcfirst(str_replace(' ', '-', 'And a fifth one')),
+    ],
+];
+
+require('about.tpl.php');
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+$title = 'My blog :: Home';
+
+$posts = [
+    1 => [
+        'title' => 'Title 1',
+        'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content',
+        'slug' => 'title-1',
+    ],
+    2 => [
+        'title' => 'Title 2',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequuntur est hic, impedit porro tempore?',
+        'slug' => 'title-2',
+    ],
+    3 => [
+        'title' => 'Title 3',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos ea hic iusto, modi quaerat tempore tenetur unde vitae. Accusamus commodi debitis nemo tempore. Aliquam dicta eius ex nihil quo!',
+        'slug' => 'title-3',
+    ],
+    4 => [
+        'title' => 'Title 4',
+        'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content',
+        'slug' => 'title-4',
+    ],
+    5 => [
+        'title' => 'Title 5',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, minima.',
+        'slug' => 'title-5',
+    ],
+];
+
+$recent_posts = [
+    1 => [
+        'title' => 'An item',
+        'slug' => lcfirst(str_replace(' ', '-', 'An item')),
+    ],
+    2 => [
+        'title' => 'A second item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A second item')),
+    ],
+    3 => [
+        'title' => 'A third item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A third item')),
+    ],
+    4 => [
+        'title' => 'A fourth item',
+        'slug' => lcfirst(str_replace(' ', '-', 'A fourth item')),
+    ],
+    5 => [
+        'title' => 'And a fifth one',
+        'slug' => lcfirst(str_replace(' ', '-', 'And a fifth one')),
+    ],
+];
+
+require('index.tpl.php');
